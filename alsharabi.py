0944ff8db7357fcc4ac240f82a7a4fb48ceb07b0
sadamalsharabi <44142745+sadamshr3be@users.noreply.github.com>
2021-01-22 13:32:45 +0300

    Add files via upload

---
+++ alsharabi.py
@@ -0,0 +1,3 @@
+import m
+m.slam()
+
---
+++ install.sh
@@ -0,0 +1,11 @@
+green='\e[1;32m'
+echo -e $green
+echo "•…• •…• •…• •…• •…• •…• •…• •…•"
+echo  "~          Welcome           ~"
+sleep 0.3
+echo "======> Please wait for waiting <======"
+pkg install python2 -y
+pip2 install mechanize
+pkg install figlet -y
+clear
+ls
